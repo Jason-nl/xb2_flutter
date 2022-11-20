ea4c4ea3f8f6b437e0087c31849b17b0c4e2e6eb
zhangle <15311418932@163.com>
2022-11-20 19:44:28 +0800

    http pratice

--- lib/playground/playground.dart
+++ lib/playground/playground.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:xb2_flutter/playground/routing/playground_routing.dart';
+import 'package:xb2_flutter/playground/http/playground_http.dart';
+// import 'package:xb2_flutter/playground/routing/playground_routing.dart';
 // import 'package:xb2_flutter/playground/input/playground_input.dart';
 // import 'package:xb2_flutter/playground/layout/playground_layout.dart';
 // import 'package:xb2_flutter/playground/state/playground_state.dart';
@@ -8,6 +9,6 @@ class PlayGroud extends StatelessWidget {
   const PlayGroud({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const PlayGroundRouting();
+    return const PlayGroundHttp();
   }
 }
